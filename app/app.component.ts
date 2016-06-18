edc4b847faa78f9aff0ee2b05691830942531559
Jason Levinsohn <jason.levinsohn@gmail.com>
2016-06-18 19:06:14 -0400

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'app',
+    template: `
+    <p>Hello Homies</p>
+    `
+})
+
+export class AppComponent {
+    constructor() {}
+}
